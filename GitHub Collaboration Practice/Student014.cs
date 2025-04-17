a29258dfa3bbf63869605749998d0725f4ba4426
chuck-w-moore <moore3cw@mail.uc.edu>
2025-04-17 11:55:51 -0400

    Added my method to Student014 class.

--- GitHub Collaboration Practice/Student014.cs
+++ GitHub Collaboration Practice/Student014.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace GitHub_Collaboration_Practice
 {
     internal class Student014
     {
+        public static void Hello(string message)
+        {
+            message = "hello";
+            Console.WriteLine(message);
+        }
     }
 }
